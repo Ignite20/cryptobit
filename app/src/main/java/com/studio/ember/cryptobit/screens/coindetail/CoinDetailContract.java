b5ff985d7280c07aea8cea3908a337926683ab81
ignite20 <blackhole45>
2018-07-07 22:28:41 +0200

    Comments

--- app/src/main/java/com/studio/ember/cryptobit/screens/coindetail/CoinDetailContract.java
+++ app/src/main/java/com/studio/ember/cryptobit/screens/coindetail/CoinDetailContract.java
@@ -7,14 +7,29 @@ import com.studio.ember.cryptobit.model.Coin;
 public interface CoinDetailContract {
     interface View extends BaseView<CoinDetailContract.Presenter> {
 
+        /**
+         * method to show info from the coin object in the UI
+         * @param coin
+         */
         void showInfo(Coin coin);
     }
 
     interface Presenter extends BasePresenter {
+        /**
+         * Set data sent by Coin Screen
+         * @param coin The coin object to be set
+         */
         void setData(Coin coin);
 
+        /**
+         * Change market coin to update info in the details screen
+         * @param coin the market coin to be updated
+         */
         void changeMarketCoin(String coin);
 
+        /**
+         * Returns the market coin string
+         */
         String getMarketCoin();
     }
 }
